0396ad118a0e2c4baa8d2b25e1d7c5ddc76a35e7
LePhanDucMan <lpdman2532@apcs.fitus.edu.vn>
2026-03-21 23:29:16 +0700

    Add files via upload

--- Heap/include/Heap.hpp
+++ Heap/include/Heap.hpp
@@ -9,15 +9,23 @@ private:
     void upHeap(int id);
     void downHeap(int id);
     void resize();
-public:
+public: // basic interfaces
     Heap();
     Heap(int capacity);
+    Heap(const Heap& other);
+    Heap& operator=(const Heap& other);
     ~Heap();
+    void swapAt(int a, int b);
     void push(int x);
     bool pop(int& x); 
+    void pop();
+    int top() const;
     bool search(int x) const;
     bool empty() const;
     int getSize() const;
+    void removeLast();
     void clear();
     int operator[](int id) const;
+public: // multistep helpers
+    void pushRaw(int x);
 };
