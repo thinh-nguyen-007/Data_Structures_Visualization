c65758c8548f2f8465fe14c456f970b6e5ac1131
KhangMatWifi <giakhang08082007@gmail.com>
2026-04-25 18:18:25 +0700

    play button and bind action

--- Visualization/main.cpp
+++ Visualization/main.cpp
@@ -33,4 +33,22 @@ int main() {
     // ===== UI =====
     InputBox input({ 50, 40 }, { 250, 45 }, font);
     OutputBox output({ 320, 40 }, { 350, 45 }, font, sf::Color::White, "Message");
+
+    // ===== BUTTONS =====
+    Button btnPlay({ 60, 120 }, 22, ButtonIcon::Play);
+    Button btnStepF({ 120, 120 }, 22, ButtonIcon::StepForward);
+    Button btnStepB({ 180, 120 }, 22, ButtonIcon::StepBackward);
+    Button btnSkipF({ 240, 120 }, 22, ButtonIcon::SkipForward);
+    Button btnSkipB({ 300, 120 }, 22, ButtonIcon::SkipBackward);
+
+    Button btnInsert({ 380, 120 }, 22, ButtonIcon::Insert);
+    Button btnDelete({ 440, 120 }, 22, ButtonIcon::Delete);
+    Button btnSearch({ 500, 120 }, 22, ButtonIcon::Search);
+
+    // ===== BIND ACTION =====
+    btnPlay.setAction(&controller, &LinkedListController::togglePaused);
+    btnStepF.setAction(&controller, &LinkedListController::nextStep);
+    btnStepB.setAction(&controller, &LinkedListController::undo);
+    btnSkipF.setAction(&controller, &LinkedListController::runToEnd);
+    btnSkipB.setAction(&controller, &LinkedListController::resetToStart);
 }
